a6257b38da6c7d8be8918fdc1682b916f34bf6c8
kamalcodezen <kamaluddin7908@gmail.com>
2026-05-27 15:31:12 +0530

    user data access

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,3 +1,4 @@
+
 import LoginForm from "@/components/auth/LoginForm";
 
 const LoginPage = () => {
--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,51 +1,99 @@
-import Link from "next/link";
+"use client";
+
 import "@/app/(auth)/auth.css";
 
-const LoginForm = () => {
+import { Eye, EyeOff } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+const LoginFrom = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  // LOGIN
+  const handleLogin = async (data) => {
+    console.log(data)
+  };
+
   return (
     <div className="w-11/12 mx-auto">
-      <div className=" flex flex-col items-center justify-center">
+      <div className="min-h-[80vh] flex flex-col items-center justify-center">
         <form
-          className=" px-8 p-20  rounded-xl  
-            max-w-md       
-            border border-zinc-200/60 dark:border-white/10
+          onSubmit={handleSubmit(handleLogin)}
+          className=" px-8 p-20 border   border-zinc-200/60 dark:border-white/10
             bg-white/80 dark:bg-zinc-900/70
             backdrop-blur-xl
             shadow-[0_8px_30px_rgb(0,0,0,0.08)]
-            dark:shadow-[0_8px_30px_rgb(0,0,0,0.35)]          
-            transition-all
-            duration-300"
+            dark:shadow-[0_8px_30px_rgb(0,0,0,0.35)] rounded-xl"
         >
           <p className="font-semibold text-2xl text-orange-600 mb-7">
             Login your account
           </p>
           <div className="mb-6">
             <div className="input-group ">
-              <input type="email" placeholder=" " />
+              <input
+                type="email"
+                {...register("email", { required: "kamal@exmaple.com*" })}
+                placeholder=" "
+              />
               <label>Email Address</label>{" "}
             </div>
+            {errors.email && (
+              <p className="text-[12px] text-red-700 mt-2">
+                {errors.email?.message}
+              </p>
+            )}
           </div>
           <div className="mb-2">
             <div className="input-group ">
-              <input type="password" placeholder=" " />
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder=" "
+                {...register("password", {
+                  required:
+                    "Must be at least 6 characters with 1 uppercase and 1 number*",
+                    
+                })}
+              />
               <label>Password</label>
-              <span className="absolute right-2 top-2 text-gray-600 "></span>
+              <span
+                className="absolute right-2 top-2 text-gray-600 "
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? (
+                  <Eye className="w-4.5" />
+                ) : (
+                  <EyeOff className="w-4.5" />
+                )}
+              </span>
             </div>
+            {errors.password && (
+              <p className="text-[12px] text-red-700 mt-2">
+                {errors.password.message}
+              </p>
+            )}
           </div>
           <p className="text-[12px] text-gray-600 underline hover:text-orange-600 mb-3">
             <Link href="">Forgot password</Link>
           </p>
           <button
             type="submit"
-            className="btn w-full bg-linear-to-t bg-gradient-to-r from-red-600 via-red-500 to-orange-500 text-white mb-3   disabled:cursor-not-allowed
+            disabled={loading}
+            className="btn w-full bg-linear-to-t  from-red-600 via-red-500 to-orange-500 text-white mb-3   disabled:cursor-not-allowed
               disabled:opacity-70"
           >
-            Login
+            login
           </button>
           <p className="text-sm text-center text-gray-700 ">
             Don’t have an account ?
             <span className="text-orange-600">
-              <Link href={""}> Register</Link>
+              <Link href={"/register"}> Register</Link>
             </span>
           </p>
         </form>
@@ -53,5 +101,4 @@ const LoginForm = () => {
     </div>
   );
 };
-
-export default LoginForm;
+export default LoginFrom;
